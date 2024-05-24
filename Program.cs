9666cc872c4e543fbdbe8084b5db1b39ed157585
eldar23451 <liyev8024@gmail.com>
2024-05-24 11:24:35 +0400

    task 10

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Metrics;
+#region Bornyear
+//int BornYear(int age, int year)
+//{
+//    Console.WriteLine("please enter your born year");
+//    return age-year;
+//}
+//Console.WriteLine(BornYear(2024, 2005));
+#endregion
+//void FindName()
+//{
+//    int counter = 1;
+//    for (int i = 0; i <=5; i++)
+//    {
+
+//    }
+//    {
+//        Console.WriteLine("please enter your name");
+//    }    string name=Console.ReadLine();
+//    if (string.IsNullOrWhiteSpace(name))
+//    {
+//        Console.WriteLine("wrong format");
+//    }     counter++;
+//         Console.WriteLine($"welcome{name}");
+//    counter = 6;
+//} FindName();
